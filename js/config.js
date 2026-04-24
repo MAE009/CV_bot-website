ad6d40e2de3533802a17717d701dea533f5e5886
Emmanuel <golgame44@gmail.com>
2026-04-24 15:50:15 +0100

    carousel template et category

--- js/config.js
+++ js/config.js
@@ -18,25 +18,25 @@ window.COLORS = {
 // Configuration des templates
 window.TEMPLATES_CONFIG = {
     ats: [
-        { image: "Assets/Cv/ATS/ats.jpg", titre: "Pro", desc: "Structure claire et professionnelle", template: "ats", category: "ATS" },
-        { image: "Assets/Cv/ATS/ats_classique.jpg", titre: "ats_classique", desc: "", template: "ats_classique", category: "ATS" },
-        { image: "Assets/Cv/ATS/ats_moderne.jpg", titre: "ats_moderne", desc: "", template: "ats_moderne", category: "ATS" },
-        { image: "Assets/Cv/ATS/ats_minimaliste.jpg", titre: "ats_minimaliste", desc: "", template: "ats_minimaliste", category: "ATS" }
+        { image: "/Assets/Cv/ATS/ats.jpg", titre: "Pro", desc: "Structure claire et professionnelle", template: "ats", category: "ATS" },
+        { image: "/Assets/Cv/ATS/ats_classique.jpg", titre: "ats_classique", desc: "", template: "ats_classique", category: "ATS" },
+        { image: "/Assets/Cv/ATS/ats_moderne.jpg", titre: "ats_moderne", desc: "", template: "ats_moderne", category: "ATS" },
+        { image: "/Assets/Cv/ATS/ats_minimaliste.jpg", titre: "ats_minimaliste", desc: "", template: "ats_minimaliste", category: "ATS" }
     ],
     moderne: [
-        { image: "Assets/Cv/MODERNE/mod.jpg", titre: "mod", desc: "", template: "mod", category: "Moderne" },
-        { image: "Assets/Cv/MODERNE/mod_blue.jpg", titre: "mod_blue", desc: "", template: "mod_blue", category: "Moderne" },
-        { image: "Assets/Cv/MODERNE/mod_white_orange.jpg", titre: "mod_white_orange.pdf", desc: "", template: "mod_white_orange", category: "Moderne" },
-        { image: "Assets/Cv/MODERNE/cv_etudiant.jpg", titre: "cv_etudiant", desc: "", template: "cv_etudiant", category: "Moderne" },
-        { image: "Assets/Cv/MODERNE/cv_etudiant_1.jpg", titre: "cv_etudiant_1", desc: "", template: "cv_etudiant_1", category: "Moderne" },
-        { image: "Assets/Cv/MODERNE/cv_etudiant_2.jpg", titre: "cv_etudiant_2", desc: "", template: "cv_etudiant_2", category: "Moderne" },
-        { image: "Assets/Cv/MODERNE/cv_etudiant_ats.jpg", titre: "cv_etudiant_ats", desc: "", template: "cv_etudiant_ats", category: "Moderne" }
+        { image: "/Assets/Cv/MODERNE/mod.jpg", titre: "mod", desc: "", template: "mod", category: "Moderne" },
+        { image: "/Assets/Cv/MODERNE/mod_blue.jpg", titre: "mod_blue", desc: "", template: "mod_blue", category: "Moderne" },
+        { image: "/Assets/Cv/MODERNE/mod_white_orange.jpg", titre: "mod_white_orange.pdf", desc: "", template: "mod_white_orange", category: "Moderne" },
+        { image: "/Assets/Cv/MODERNE/cv_etudiant.jpg", titre: "cv_etudiant", desc: "", template: "cv_etudiant", category: "Moderne" },
+        { image: "/Assets/Cv/MODERNE/cv_etudiant_1.jpg", titre: "cv_etudiant_1", desc: "", template: "cv_etudiant_1", category: "Moderne" },
+        { image: "/Assets/Cv/MODERNE/cv_etudiant_2.jpg", titre: "cv_etudiant_2", desc: "", template: "cv_etudiant_2", category: "Moderne" },
+        { image: "/Assets/Cv/MODERNE/cv_etudiant_ats.jpg", titre: "cv_etudiant_ats", desc: "", template: "cv_etudiant_ats", category: "Moderne" }
     ],
     creatif: [
-        { image: "Assets/Cv/Crea.jpg", titre: "crea", desc: "Pour les métiers du design", template: "Crea", category: "Creative" },
-        { image: "Assets/Cv/Crea2.jpg", titre: "crea2", desc: "Hors des sentiers battus", template: "Crea2", category: "Creative" },
-        { image: "Assets/Cv/Crea3.jpg", titre: "crea3", desc: "Utilisation audacieuse de la couleur", template: "Crea3", category: "Creative" },
-        { image: "Assets/Cv/Crea4.jpg", titre: "Crea4", desc: "Espace pour vos réalisations", template: "Crea4", category: "Creative" }
+        { image: "/Assets/Cv/Crea.jpg", titre: "crea", desc: "Pour les métiers du design", template: "Crea", category: "Creative" },
+        { image: "/Assets/Cv/Crea2.jpg", titre: "crea2", desc: "Hors des sentiers battus", template: "Crea2", category: "Creative" },
+        { image: "/Assets/Cv/Crea3.jpg", titre: "crea3", desc: "Utilisation audacieuse de la couleur", template: "Crea3", category: "Creative" },
+        { image: "/Assets/Cv/Crea4.jpg", titre: "Crea4", desc: "Espace pour vos réalisations", template: "Crea4", category: "Creative" }
     ]
 };
 
